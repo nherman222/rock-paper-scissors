7f0b5bc1271d45216faf9c6ef3fb784d76e1b126
Nina Herman <nina.n.herman.22@dartmouth.edu>
2020-04-27 20:42:09 -0400

    upload files

---
+++ app.js
@@ -0,0 +1,115 @@
+const game = () => {
+  let pScore = 0;
+  let cScore = 0;
+
+  // Start the game
+  const startGame = () => {
+    const playBtn = document.querySelector(".intro button");
+    const introScreen = document.querySelector(".intro");
+    const match = document.querySelector(".match");
+
+    playBtn.addEventListener("click", () => {
+      introScreen.classList.add("fadeOut");
+      match.classList.add("fadeIn");
+    });
+  };
+
+  // Play match
+  const playMatch = () => {
+    const options = document.querySelectorAll(".options button");
+    const playerHand = document.querySelector(".player-hand");
+    const computerHand = document.querySelector(".computer-hand");
+    const hands = document.querySelectorAll(".hands img");
+
+    hands.forEach(hand => {
+      hand.addEventListener("animationend", function() {
+        this.style.animation = "";
+      });
+    });
+    // Computer options
+    const computerOptions = ["rock", "paper", "scissors"];
+    options.forEach(option => {
+      option.addEventListener("click", function() {
+        // computer choice
+        const computerNumber = Math.floor(Math.random() * 3);
+        const computerChoice = computerOptions[computerNumber];
+        setTimeout(() => {
+          compareHands(this.textContent, computerChoice);
+          // update images
+          playerHand.src = `./assets/${this.textContent}.png`;
+          computerHand.src = `./assets/${computerChoice}.png`;
+        }, 2000);
+        // animation
+        playerHand.style.animation = "shakePlayer 2s ease";
+        computerHand.style.animation = "shakeComputer 2s ease";
+      });
+    });
+  };
+
+  const updateScore = () => {
+    const playerScore = document.querySelector(".player-score p");
+    const computerScore = document.querySelector(".computer-score p");
+    playerScore.textContent = pScore;
+    computerScore.textContent = cScore;
+  };
+
+  const compareHands = (playerChoice, computerChoice) => {
+    //update text
+    const winner = document.querySelector(".winner");
+    // tie
+    if (playerChoice === computerChoice) {
+      winner.textContent = "TIE!";
+      return;
+    }
+    // rock
+    if (playerChoice === "rock") {
+      if (computerChoice === "scissors") {
+        winner.textContent = "Winner: Player!";
+        pScore++;
+        updateScore();
+        return;
+      } else {
+        winner.textContent = "Winner: Computer!";
+        cScore++;
+        updateScore();
+        return;
+      }
+    }
+
+    // paper
+    if (playerChoice === "paper") {
+      if (computerChoice === "rock") {
+        winner.textContent = "Winner: Player!";
+        pScore++;
+        updateScore();
+        return;
+      } else {
+        winner.textContent = "Winner: Computer!";
+        cScore++;
+        updateScore();
+        return;
+      }
+    }
+
+    // scissors
+    if (playerChoice === "scissors") {
+      if (computerChoice === "paper") {
+        winner.textContent = "Winner: Player!";
+        pScore++;
+        updateScore();
+        return;
+      } else {
+        winner.textContent = "Winner: Computer!";
+        cScore++;
+        updateScore();
+        return;
+      }
+    }
+  };
+
+  // call all the inner functions
+  startGame();
+  playMatch();
+};
+
+game();
